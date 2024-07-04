194e14273ebc0d116d74b4098f7b15bd16812c96
Tanvi51204 <tanvibisht2004@gmail.com>
2024-07-04 16:15:56 +0530

    second commit

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 import streamlit as st
 import pickle
 import pandas as pd
-products_list=pickle.load(open('data1.pkl','rb'))
-similarity=pickle.load(open('similarity.pkl','rb'))
+products_list=pickle.load(open('data_new.pkl','rb'))
+similarity=pickle.load(open('similarity_new.pkl','rb'))
+
 
 products_names=products_list['Name'].values
 st.title('Product recommender system')
 
-# def recommend(product):
-#     item_index =products_list[products_list['Name']==product].index[0]
-#     similar_items = list(enumerate(cosine_similarities_content[item_index]))
-#     similar_items = sorted(similar_items, key=lambda x:x[1], reverse=True)
-#     top_similar_items = similar_items[1:10]
-#     recommended_items_indics = [x[0] for x in top_similar_items]
-#     recommended_products=[]
-#     for i in top_similar_items:
-#         recommended_products.append(products_list.iloc[recommended_items_indics][['Name','ReviewCount','Brand']])
-#     return recommended_products
+
     
 def recommend(product):
     try:
